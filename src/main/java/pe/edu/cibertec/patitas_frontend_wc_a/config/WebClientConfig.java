097456cb1878aa656952108f3fb4360d197c541f
NicolasSembrera <nsembrera@gmail.com>
2024-10-13 13:37:22 -0500

    Subiendo proyecto frontend wc y Examen T2

---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/config/WebClientConfig.java
@@ -0,0 +1,29 @@
+package pe.edu.cibertec.patitas_frontend_wc_a.config;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebClientConfig {
+
+    HttpClient httpClient = HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .responseTimeout(Duration.ofSeconds(10))
+            .doOnConnected(connection -> connection.addHandlerLast(new ReadTimeoutHandler(10, TimeUnit.SECONDS)));
+
+    @Bean
+    public WebClient webClientAutenticacion(WebClient.Builder builder) {
+        return builder
+                .baseUrl("http://localhost:8081/autenticacion")
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+}
